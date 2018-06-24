102454e90613457a99423eb38578f0b8c9e90d5a
damckinnon <insaynity@gmail.com>
2018-06-23 22:42:22 -0700

    Finding and matches features

--- Main.cpp
+++ Main.cpp
@@ -1,4 +1,6 @@
 #include <opencv2/opencv.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/features2d.hpp>
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -10,53 +12,85 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-	/*
-	  So what I'm going to do is:
-	    - bring in both the images
-		- import the intrinsic camera settings
+    /*
+      So what I'm going to do is:
+        - bring in both the images
+        - import the intrinsic camera settings // lol trouble with this
 
-		- Use SIFT to compute feature descriptors
-		- Use FLANN (?) to match the descriptors (or write own algo) between the images
-		- Use the 8 point algorithm to find the essential matrix
-		- Use an epipolar search and maybe some triangulation or P3P to find the depth of some points
-		- Rectify? To get a disparity map? To optimise the epipolar search?
-	*/
+        - Use SIFT to compute feature descriptors
+        - Use FLANN (?) to match the descriptors (or write own algo) between the images
+        - Use the 8 point algorithm to find the essential matrix
+        - Use an epipolar search and maybe some triangulation or P3P to find the depth of some points
+        - Rectify? To get a disparity map? To optimise the epipolar search?
+    */
 
-	// Read in both image files
-	Mat leftImage = imread("C:\\Users\\d_mcc\\Projects\\vision\\Classroom1-perfect\\im0.png");
-	Mat rightImage = imread("C:\\Users\\d_mcc\\Projects\\vision\\Classroom1-perfect\\im1.png");
+    // Read in both image files
+    Mat leftImage = imread("C:\\Users\\d_mcc\\Projects\\vision\\Classroom1-perfect\\im0.png");
+    Mat rightImage = imread("C:\\Users\\d_mcc\\Projects\\vision\\Classroom1-perfect\\im1.png");
 
-	// Read in the camera settings
-	// TODO: Export this to another function
-	Mat leftIntrinsics(Size(3,3), CV_32F, Scalar(0));
-	Mat rightIntrinsics(Size(3, 3), CV_32F, Scalar(0));
-	float dOffset = 0.f;
-	float baseline = 0.f;
-	// Hardcoded from file cos file reading is hard????
+    // Read in the camera settings
+    // TODO: Export this to another function
+    Mat leftIntrinsics = Mat::zeros(3, 3, CV_32F);
+    Mat rightIntrinsics = Mat::zeros(3, 3, CV_32F);
+    float dOffset = 0.f;
+    float baseline = 0.f;
+
+    // Hardcoded from file cos file reading is hard????
+    // TODO: fix
+    dOffset = 107.911;
+    baseline = 237.604;
+    leftIntrinsics.at<float>(0,0) = 3962.004f;
+    leftIntrinsics.at<float>(0,1) = 0.f;
+    leftIntrinsics.at<float>(0, 2) = 1146.717f;
+    leftIntrinsics.at<float>(1, 0) = 0.f;
+    leftIntrinsics.at<float>(1, 1) = 3962.004f;
+    leftIntrinsics.at<float>(1, 2) = 975.476f;
+    leftIntrinsics.at<float>(2, 0) = 0.f;
+    leftIntrinsics.at<float>(2, 1) = 0.f;
+    leftIntrinsics.at<float>(2, 2) = 1.f;
+
+    rightIntrinsics.at<float>(0, 0) = 3962.004;
+    rightIntrinsics.at<float>(0, 1) = 0;
+    rightIntrinsics.at<float>(0, 2) = 1254.628;
+    rightIntrinsics.at<float>(1, 0) = 0;
+    rightIntrinsics.at<float>(1, 1) = 3962.004;
+    rightIntrinsics.at<float>(1, 2) = 975.476;
+    rightIntrinsics.at<float>(2, 0) = 0;
+    rightIntrinsics.at<float>(2, 1) = 0;
+    rightIntrinsics.at<float>(2, 2) = 1;
+    //////////////////ugh/////////////////////////
+
+    // Create feature descriptor vectors for each of the images
+	// We use ORB features
+	vector<KeyPoint> keypointsLeft, keypointsRight;
+	Mat descLeft, descRight;
+	Ptr<ORB> detector = ORB::create();
+	detector->detectAndCompute(leftImage, noArray(), keypointsLeft, descLeft);
+	detector->detectAndCompute(rightImage, noArray(), keypointsRight, descRight);
+
+    // Brute force matcher, cos screw compute time, we'll figure this out later
+    BFMatcher matcher;
+    vector<DMatch> matches;
+    matcher.match(descLeft, descRight, matches);
+
+    // debug - draw matches
+	Mat output;
+	drawMatches(leftImage, keypointsLeft, rightImage, keypointsRight, matches, output);
 
 
-	// Read the image file
-	Mat image = imread("C:\\Users\\d_mcc\\OneDrive\\Pictures\\pirate.png");
+    std::string leftWindowName = "Left image";
+    std::string rightWindowName = "Right image";
+	std::string debugWindowName = "debug image";
 
-	// Check for failure
-	if (image.empty())
-	{
-		cout << "Could not open or find the image" << endl;
-		cin.get(); //wait for any key press
-		return -1;
-	}
+    namedWindow(debugWindowName); // Create a window
 
-	std::string windowName = "The Guitar"; //Name of the window
+    imshow(debugWindowName, output); // Show our image inside the created window.
 
-	namedWindow(windowName); // Create a window
+    waitKey(0); // Wait for any keystroke in the window
 
-	imshow(windowName, image); // Show our image inside the created window.
+    destroyWindow(debugWindowName); //destroy the created window
 
-	waitKey(0); // Wait for any keystroke in the window
-
-	destroyWindow(windowName); //destroy the created window
-
-	return 0;
+    return 0;
 }
 
 // Read camera params
