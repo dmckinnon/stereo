030b51a4964fb84d01a1c9bb502236e159dc6829
damckinnon <insaynity@gmail.com>
2019-11-13 20:08:43 -0800

    Cleaned up, finished triangulation section

--- stereo/main.cpp
+++ stereo/main.cpp
@@ -41,12 +41,6 @@ using namespace Eigen;
 #define RECTIFICATION_DEPTH_MAP
 
 
-/*GLuint buffer = 0;
-GLuint vPos;
-GLuint program;
-
-GLuint numPoints = 0;*/
-
 /*
 	This is an exercise in stereo depth-maps and reconstruction (stretch goal)
 	See README for more detail
@@ -82,18 +76,12 @@ GLuint numPoints = 0;*/
 		
 	Depth-map
 
+	TODO:
+	- rectification
+	- write up Lindstrom
+
 */
-/*
-void initWithPoints(const std::vector<Vector3f>& points);
-//void reshape(int width, int height);
-//void display();
-void reshape (int w, int h);
-void renderScene(void);
-void processKeys(int key, int xx, int yy);
-float angle, lx, lz, x, z;
-//GLfloat translateX, translateY, translateZ;
-//GLfloat rotateX, rotateY, rotateZ;
-vector<Vector3f> pointsToDraw;*/
+
 // Support functions
 vector<string> get_all_files_names_within_folder(string folder)
 {
@@ -293,9 +281,50 @@ int main(int argc, char** argv)
 	vector<std::pair<Feature, Feature>> newMatches;
 	newMatches = MatchDescriptors(images[0].features, images[1].features, MAX_DIST_BETWEEN_MATCHES*2);
 
+	// COmpute transform between images
+	Vector3f t(0, 0, 0);
+	Matrix3f R;
+	R.setZero();
+	DecomposeEssentialMatrix(stereo.E, R, t);
+	
 	// Now for each match, get the depth in the frame of the first image
+	vector<Vector3f> depthPoints;
+	for (auto& match : newMatches)
+	{
+		Point2f xprime = match.first.p;
+		Point2f x = match.second.p;
+		Vector3f pointX = stereo.img2.K.inverse() * Vector3f(x.x, x.y, 1);
+		Vector3f pointXPrime = stereo.img1.K.inverse() * Vector3f(xprime.x, xprime.y, 1);
+		float d0 = 0;
+		float d1 = 0;
+		// TODO - do I need to do this the other way?
+		// TODO - normalise or no? No?
+		Matrix3f Einverse = stereo.E.inverse();
+		if (!Triangulate(d0, d1, pointX, pointXPrime, stereo.E))
+		{
+			match.first.depth = BAD_DEPTH;
+			match.second.depth = BAD_DEPTH;
+			continue;
+		}
 
-	/*
+		// Is this depth in first frame or second frame?
+		// There is also a bug where depth is negative sometimes.
+		// This can happen from triangulation
+		d0 = abs(d0);
+		d1 = abs(d1);
+		match.first.depth = abs(d0);
+		match.second.depth = abs(d1);
+
+		Vector3f pointIn3D(xprime.x, xprime.y, 1);
+		pointIn3D = images[0].K.inverse() * pointIn3D;
+		pointIn3D = pointIn3D / pointIn3D[2];
+		pointIn3D *= d1;
+
+		depthPoints.push_back(pointIn3D);
+	}
+
+
+	
 	// Write these points out to a text file
 	if (pointCloudOutputPath.size() == 0)
 	{
@@ -305,21 +334,21 @@ int main(int argc, char** argv)
 	std::ofstream pointFile(pointCloudOutputPath + "\\point_cloud.txt");
 	if (pointFile.is_open())
 	{
-		for (int i = 0; i < pointsToDraw.size(); ++i)
+		for (int i = 0; i < depthPoints.size(); ++i)
 		{
-			auto& p = pointsToDraw[i];
+			auto& p = depthPoints[i];
 			// get normals
 			Vector3f normal = p;
 			normal *= 1 / sqrt(normal[0] * normal[0] + normal[1]*normal[1] + normal[2]*normal[2]);
 			pointFile << p[0] << " " << p[1] << " " << p[2] << " " << normal[0] << " " << normal[1] << " " << normal[2];
-			if (i < pointsToDraw.size() - 1)
+			if (i < depthPoints.size() - 1)
 			{
 				pointFile << endl;
 			}
 		}
 		pointFile.close();
 	}
-	*/
+	
 #endif
 
 #ifdef RECTIFICATION_DEPTH_MAP
@@ -338,159 +367,6 @@ int main(int argc, char** argv)
 
 	// save depth map?
 #endif
-	for (auto& match : matches)
-	{
-		Mat epipolarLines;
-		Mat img_1 = imread(images[0].filename, IMREAD_GRAYSCALE);
-		Mat img_2 = imread(images[1].filename, IMREAD_GRAYSCALE);
-		hconcat(img_1, img_2, epipolarLines);
-		int offset = img_1.cols;
-		// depth is way the hell off. Fix this anohter time
-
-		// TODO: triangulation isn't working. 
-		// or it gives negative depth
-		// Theory 1: mathematics is wrong somehow, not sure where
-		// Can show this in OpenGL to debug easier?
-
-		// Potential issues:
-		// - coordinate normalisation?
-		// - need to anti-rotate t vector? (doubt it)
-		// - decomposition of E into R and t? This seems weird and too convenient
-
-		Point2f xprime = match.first.p;
-		Point2f x = match.second.p;
-		Vector3f pointX = stereo.img2.K.inverse() * Vector3f(x.x, x.y, 1);
-		Vector3f pointXPrime = stereo.img1.K.inverse() * Vector3f(xprime.x, xprime.y, 1);
-		float d0 = 0;
-		float d1 = 0;
-		// TODO - do I need to do this the other way?
-		// TODO - normalise or no? No?
-		Matrix3f Einverse = stereo.E.inverse();
-		if (!Triangulate(d0, d1, pointX, pointXPrime, stereo.E))
-		{
-			match.first.depth = BAD_DEPTH;
-			match.second.depth = BAD_DEPTH;
-			continue;
-		}
-
-		circle(epipolarLines, xprime, 3, (255, 255, 0), -1);
-		x.x += offset;
-		circle(epipolarLines, x, 3, (255, 255, 0), -1);
-
-		// Test: if we can triangulate, then reproject from camera 1 to camera 0
-		// and check reprojection error - this should weed out bad matches
-		/*Point2f img1Point = match.second.p;
-		Vector3f projectivePoint;
-		projectivePoint[0] = img1Point.x*4;
-		projectivePoint[1] = img1Point.y*4;
-		projectivePoint[2] = 1;
-		Vector3f point = images[1].K.inverse() * projectivePoint;
-		point = point / point[2];
-		point *= abs(d1); // TODO: fix up depth algo
-		// Now apply R and t to get to cam0 frame:
-		Vector3f t(0, 0, 0);
-		Matrix3f R;
-		R.setZero();
-		DecomposeEssentialMatrix(stereo.E, R, t);
-		Vector3f transformedPoint = R * point + t; // IS THIS RIGHT?
-		// remove depth:
-		transformedPoint /= transformedPoint[2];
-		// now project:
-		projectivePoint = images[0].K * transformedPoint;
-		// get u, v from first to bits
-		Point2f reprojection(projectivePoint[0], projectivePoint[1]);*/
-		// Now compare to the original
-		//cout << "Comparing " << reprojection << " to " << match.first.p.x << ", " << match.first.p.y << endl;
-
-
-		// Now transform to cam 0
-
-		// Is this depth in first frame or second frame?
-		d0 = abs(d0);
-		d1 = abs(d1);
-		match.first.depth = abs(d0);
-		match.second.depth = abs(d1);// transform the point in 3D from first camera to second camera
-		cout << "Depths are " << d0 << " and " << d1 << endl;
-
-		Vector3f t(0, 0, 0);
-		Matrix3f R;
-		R.setZero();
-		DecomposeEssentialMatrix(stereo.E, R, t);
-		Vector3f projectivePoint;
-		projectivePoint[0] = xprime.x;
-		projectivePoint[1] = xprime.y;
-		projectivePoint[2] = 1;
-		Vector3f point = images[0].K.inverse() * projectivePoint;
-		point = point / point[2];
-		Vector3f eL = point * d1;
-		Vector3f transformedPoint = R.inverse() * eL - R.inverse() * t; // IS THIS RIGHT?
-		transformedPoint /= transformedPoint[2];
-		//cout << "normalised:\n" << transformedPoint << endl;
-		// now project:
-		projectivePoint = images[1].K * transformedPoint;
-		// get u, v from first to bits
-		Point2f reprojection(projectivePoint[0], projectivePoint[1]);
-
-
-
-
-
-		// Transform points into each camera frame
-
-		// Copy the depths to the StereoPair array
-		for (auto& f : images[0].features)
-		{
-			if (f == match.first)
-			{
-				f.depth = match.first.depth;
-			}
-		}
-		for (auto& f : images[1].features)
-		{
-			if (f == match.second)
-			{
-				//f.depth = match.second.depth;
-			}
-		}
-
-		reprojection.x += offset;
-		circle(epipolarLines, reprojection, 5, (255, 255, 0), 2);
-		cout << "Point at depth " << d0 << " is " << reprojection << endl;
-
-		// Display
-		imshow("Depths", epipolarLines);
-		waitKey(0);
-	}
-	
-
-	// Render points
-	vector<Vector3f> pointsToDraw; 
-	pointsToDraw.clear();
-	for (int i = 0; i < s; ++i)
-	{
-		for (auto& f : images[i].features)
-		{
-			if (abs(f.depth) > 1000)
-			{
-				// remove nonsense
-				continue;
-			}
-
-			Vector3f projectivePoint;
-			projectivePoint[0] = f.p.x;
-			projectivePoint[1] = f.p.y;
-			projectivePoint[2] = 1;
-			Vector3f point = images[i].K.inverse()*projectivePoint;
-			point = point / point[2];
-			point *= f.depth;
-			pointsToDraw.push_back(point);
-		}
-
-		// Only use the first image points
-		break;
-	}
-
-	
 
 	return 0;
 }
@@ -617,249 +493,106 @@ void DebugEpipolarLines(
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* ############################################################################
-    OpenGL section below
-   ############################################################################ */
-
 /*
-	OpenGL helpers for drawing
-	I couldn't figure out how to have this in a different file, so it's all here
-*/
-/*
-void initWithPoints(const std::vector<Vector3f>& points)
+	The following is a loop that draws the epipolar lines, so you can see what this
+	looks like
+
+
+for (auto& match : matches)
 {
-	GLfloat* vertices = (GLfloat*)malloc(sizeof(GLfloat*)*points.size()*4);
-	for (size_t i = 0; i < points.size(); ++i)
+	Mat epipolarLines;
+	Mat img_1 = imread(images[0].filename, IMREAD_GRAYSCALE);
+	Mat img_2 = imread(images[1].filename, IMREAD_GRAYSCALE);
+	hconcat(img_1, img_2, epipolarLines);
+	int offset = img_1.cols;
+	// depth is way the hell off. Fix this anohter time
+
+	// TODO: triangulation isn't working.
+	// or it gives negative depth
+	// Theory 1: mathematics is wrong somehow, not sure where
+	// Can show this in OpenGL to debug easier?
+
+	// Potential issues:
+	// - coordinate normalisation?
+	// - need to anti-rotate t vector? (doubt it)
+	// - decomposition of E into R and t? This seems weird and too convenient
+
+	Point2f xprime = match.first.p;
+	Point2f x = match.second.p;
+	Vector3f pointX = stereo.img2.K.inverse() * Vector3f(x.x, x.y, 1);
+	Vector3f pointXPrime = stereo.img1.K.inverse() * Vector3f(xprime.x, xprime.y, 1);
+	float d0 = 0;
+	float d1 = 0;
+	// TODO - do I need to do this the other way?
+	// TODO - normalise or no? No?
+	Matrix3f Einverse = stereo.E.inverse();
+	if (!Triangulate(d0, d1, pointX, pointXPrime, stereo.E))
 	{
-		vertices[4*i] = points[i][0];
-		vertices[4*i+1] = points[i][1];
-		vertices[4*i+2] = points[i][2];
-		vertices[4*i+3] = 1;
+		match.first.depth = BAD_DEPTH;
+		match.second.depth = BAD_DEPTH;
+		continue;
 	}
-	numPoints = (GLuint)points.size();
-	angle = 0;
-	lx = 0;
-	lz = 0;
-	x = 0;
-	z = 0;
+
+	circle(epipolarLines, xprime, 3, (255, 255, 0), -1);
+	x.x += offset;
+	circle(epipolarLines, x, 3, (255, 255, 0), -1);
+
+	// Now transform to cam 0
+
+	// Is this depth in first frame or second frame?
+	d0 = abs(d0);
+	d1 = abs(d1);
+	match.first.depth = abs(d0);
+	match.second.depth = abs(d1);// transform the point in 3D from first camera to second camera
+	cout << "Depths are " << d0 << " and " << d1 << endl;
+
+	Vector3f t(0, 0, 0);
+	Matrix3f R;
+	R.setZero();
+	DecomposeEssentialMatrix(stereo.E, R, t);
+	Vector3f projectivePoint;
+	projectivePoint[0] = xprime.x;
+	projectivePoint[1] = xprime.y;
+	projectivePoint[2] = 1;
+	Vector3f point = images[0].K.inverse() * projectivePoint;
+	point = point / point[2];
+	Vector3f eL = point * d1;
+	Vector3f transformedPoint = R.inverse() * eL - R.inverse() * t; // IS THIS RIGHT?
+	transformedPoint /= transformedPoint[2];
+	//cout << "normalised:\n" << transformedPoint << endl;
+	// now project:
+	projectivePoint = images[1].K * transformedPoint;
+	// get u, v from first to bits
+	Point2f reprojection(projectivePoint[0], projectivePoint[1]);
 
 
-	// Get an unused buffer object name. Required after OpenGL 3.1. 
-	glGenBuffers(1, &buffer);
-
-	// If it's the first time the buffer object name is used, create that buffer. 
-	glBindBuffer(GL_ARRAY_BUFFER, buffer);
-
-	// Allocate memory for the active buffer object. 
-	// 1. Allocate memory on the graphics card for the amount specified by the 2nd parameter.
-	// 2. Copy the data referenced by the third parameter (a pointer) from the main memory to the 
-	//    memory on the graphics card. 
-	// 3. If you want to dynamically load the data, then set the third parameter to be NULL. 
-	glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat)*4*points.size(), vertices, GL_DYNAMIC_DRAW);
-
-	// OpenGL vertex shader source code
-	const char* vSource = {
-		"#version 330\n"
-		"in vec4 vPos;"
-		"void main() {"
-		"	gl_Position = vPos * vec4(1.0f, 1.0f, 1.0f, 1.0f);"
-		"}"
-	};
-
-	// OpenGL fragment shader source code
-	const char* fSource = {
-		"#version 330\n"
-		"out vec4 fragColor;"
-		"void main() {"
-		"	fragColor = vec4(0.8, 0.8, 0, 1);"
-		"}"
-	};
-
-	// Declare shader IDs
-	GLuint vShader, fShader;
-
-	// Create empty shader objects
-	vShader = glCreateShader(GL_VERTEX_SHADER);
-	fShader = glCreateShader(GL_FRAGMENT_SHADER);
-
-	// Attach shader source code the shader objects
-	glShaderSource(vShader, 1, &vSource, NULL);
-	glShaderSource(fShader, 1, &fSource, NULL);
-
-	// Compile shader objects
-	glCompileShader(vShader);
-	glCompileShader(fShader);
-
-	// Create an empty shader program object
-	program = glCreateProgram();
-
-	// Attach vertex and fragment shaders to the shader program
-	glAttachShader(program, vShader);
-	glAttachShader(program, fShader);
-
-	// Link the shader program
-	glLinkProgram(program);
-
-	// Retrieve the ID of a vertex attribute, i.e. position
-	vPos = glGetAttribLocation(program, "vPos");
-
-	// Specify the background color
-	glClearColor(0, 0, 0, 1);
-}
-
-void reshape(int width, int height)
-{
-	// Compute aspect ratio of the new window
-	if (height == 0) height = 1;                // To prevent divide by 0
-	GLfloat aspect = (GLfloat)width / (GLfloat)height;
-
-	// Set the viewport to cover the new window
-	glViewport(0, 0, width, height);
-
-	// Set the aspect ratio of the clipping volume to match the viewport
-	glMatrixMode(GL_PROJECTION);  // To operate on the Projection matrix
-	glLoadIdentity();             // Reset
-	// Enable perspective projection with fovy, aspect, zNear and zFar
-	gluPerspective(45.0f, aspect, 0.1f, 100.0f);
-}
-
-void display()
-{
-	// Clear the window with the background color
-	glClear(GL_COLOR_BUFFER_BIT);
-
-	glMatrixMode(GL_MODELVIEW); //set the matrix to model view mode
-
-    glPushMatrix(); // push the matrix
-
-	// Activate the shader program
-	glUseProgram(program);
-
-	// If the buffer object already exists, make that buffer the current active one. 
-	// If the buffer object name is 0, disable buffer objects. 
-	glBindBuffer(GL_ARRAY_BUFFER, buffer);
-
-	// Associate the vertex array in the buffer object with the vertex attribute: "position"
-	glVertexAttribPointer(vPos, 4, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(0));
-
-	// Enable the vertex attribute: "position"
-	glEnableVertexAttribArray(vPos);
-
-	// Start the shader program
-	glDrawArrays(GL_POINTS, 0, numPoints);
-
-	// Are these necessary?
-	glDisableVertexAttribArray(0);
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-	glPopMatrix();//pop the matrix
-
-    glMatrixMode(GL_PROJECTION); // Apply projection matrix again
-
-	// Refresh the window
-	glutSwapBuffers();
-}
-
-void processKeys(int key, int xx, int yy)
-{
-	switch (key) {
-		case GLUT_KEY_LEFT :
-			rotateY -= ROTATION_STEP;
-			break;
-		case GLUT_KEY_RIGHT :
-			rotateY += ROTATION_STEP;
-			break;
-		case GLUT_KEY_UP :
-			rotateX += ROTATION_STEP;
-			break;
-		case GLUT_KEY_DOWN :
-			rotateX -= ROTATION_STEP;
-			break;
-		case GLUT_KEY_CTRL_L:
-			translateX += TRANSLATION_STEP;
-			break;
-		case GLUT_KEY_CTRL_R:
-			translateX -= TRANSLATION_STEP;
-			break;
-		case GLUT_KEY_ALT_L:
-			translateY += TRANSLATION_STEP;
-			break;
-		case GLUT_KEY_ALT_R:
-			translateY -= TRANSLATION_STEP;
-			break;
-		case GLUT_KEY_SHIFT_L:
-			translateZ += TRANSLATION_STEP;
-			break;
-		case GLUT_KEY_SHIFT_R:
-			translateZ -= TRANSLATION_STEP;
-			break;
-		case GLUT_KEY_END :
-			// reset all values
-			translateX = 0.f;
-			translateY = 0.f;
-			translateZ = 0.f;
-			rotateX = 0.f;
-			rotateY = 0.f;
-			rotateZ = 0.f;
-			break;
-	}
-}
-
-void reshape (int w, int h) 
-{
-	glViewport (0, 0, (GLsizei)w, (GLsizei)h);  
-	glMatrixMode (GL_PROJECTION);  
-	glLoadIdentity ();  
-	gluPerspective (60, (GLfloat)w / (GLfloat)h, 1.0, 500.0);
-	glMatrixMode (GL_MODELVIEW);  
-}
-
-void renderScene(void)
-{
-	glClear (GL_COLOR_BUFFER_BIT);  
-	glLoadIdentity();
-	// Now update transformation matrices with keys
-
-	// translation
-	glTranslatef(translateX, translateY, translateZ);
-
-	// rotation
-	glRotatef(rotateX, 1.0, 0.0, 0.0); //rotate about the x axis
-	glRotatef(rotateY, 0.0, 1.0, 0.0); //rotate about the y axis
-	glRotatef(rotateZ, 0.0, 0.0, 1.0); //rotate about the z axis
-
-	
 
 
-	float x,y,z;  
-	glPointSize(2.0);   
-	glBegin(GL_POINTS);  
-	for (const auto& p : pointsToDraw)
+
+	// Transform points into each camera frame
+
+	// Copy the depths to the StereoPair array
+	for (auto& f : images[0].features)
 	{
-		glVertex3f(p[0],p[1],p[2]);  
+		if (f == match.first)
+		{
+			f.depth = match.first.depth;
+		}
+	}
+	for (auto& f : images[1].features)
+	{
+		if (f == match.second)
+		{
+			//f.depth = match.second.depth;
+		}
 	}
 
-	glEnd();  
-	glFlush();  
+	reprojection.x += offset;
+	circle(epipolarLines, reprojection, 5, (255, 255, 0), 2);
+	cout << "Point at depth " << d0 << " is " << reprojection << endl;
 
-	glutSwapBuffers();
+	// Display
+	imshow("Depths", epipolarLines);
+	waitKey(0);
 }
 */
